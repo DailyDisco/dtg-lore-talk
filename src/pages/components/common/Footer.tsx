6e12e2cbc0ba89ac262729f3fc5332560f53e62a
boonaki <joshdurr17@gmail.com>
2023-03-15 15:18:15 -0400

    breaking changes

---
+++ src/pages/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+// const Footer = () => {
+//   return (
+//     <footer className="min-h-100vh flex flex-col">
+//       <div className="relative w-full">
+//         <div className="absolute w-full text-center">
+//           <h2 className="font-bold text-3xl">DTG Lore Talk</h2>
+//       </div>
+//     </div>
+//   </footer>
+//   )
+// }
+
+// export default Footer
---
+++ src/pages/components/common/Template.tsx
@@ -0,0 +1,11 @@
+export interface IBaseTemplate {
+    sampleTextProp: string;
+  }
+
+const Template: React.FC = () => {
+  return (
+    <div>Template</div>
+  )
+}
+
+export default Template
