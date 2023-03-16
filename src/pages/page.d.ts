33bd49c44e78099ed453a9312f67c8435a7c0e06
DailyDisco <actual.mensch0r@icloud.com>
2023-03-16 14:00:11 -0700

    add Layout fix merge changes
    Co-authored-by: Josh Durrett <boonaki@users.noreply.github.com>

---
+++ src/pages/page.d.ts
@@ -0,0 +1,14 @@
+// This is file allows us to set a default layout for a page. This is useful for pages that are not wrapped in a layout component.
+
+// We can also take in a layout as a argument to the page component. This is useful for pages that are wrapped in a layout component.
+
+import { NextPage } from "next";
+import { ComponentType, ReactElement, ReactNode } from "react";
+
+// Defines the NextPageWithLayout type
+export type NextPageWithLayout<P = {}> = NextPage<P> & {
+  // The getLayout function wraps a page component with a layout component
+  getLayout?: (_page: ReactElement) => ReactNode;
+  // The layout property specifies which layout component to use
+  layout?: ComponentType;
+};
