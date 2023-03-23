80466a4128fed9601d27d85fff7d0bf08d78e296
Diego (D) <actual.mensch0r@icloud.com>
2023-03-23 14:03:45 -0500

    feat: add icons to rows, remove titles section

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -3,17 +3,9 @@ import CardRow from "./CardRow";
 
 const Card = ({ threads }: { threads: any }) => {
   return (
-    <div className="rounded-2xl border-4 border-slate-700 bg-slate-500 p-4 text-white shadow-lg shadow-black flex flex-col mx-4 mt-4">
-      <h1 className="bold text-4xl mb-4">Threads</h1>
+    <div className="mx-4 mt-4 flex flex-col rounded-2xl border-4 border-slate-700 bg-slate-500 p-4 text-white shadow-lg shadow-black">
+      <h1 className="bold mb-4 text-4xl">Threads</h1>
       {/* <div className="mb-7 h-1 bg-black"></div> */}
-
-      <div className="flex text-lg justify-around mb-2">
-        <h3>User</h3>
-        <h3>Title</h3>
-        <h3>Author</h3>
-        <h3 className="hidden md:block">Views</h3>
-        <h3 className="hidden md:block">Replies</h3>
-      </div>
 
       <section className="w-full">
         {threads.map((thread: { node: any }, index: Key | null | undefined) => (
--- src/components/home/CardRow.tsx
+++ src/components/home/CardRow.tsx
@@ -1,28 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import {
+  ArrowRightOnRectangleIcon,
+  ChatBubbleLeftIcon,
+  EyeIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 
 const CardRow = ({ threads }: { threads: any }) => {
   return (
-    <div className="flex items-center justify-around rounded-xl bg-white p-3 text-black my-2">
-      <Link href={`/user/profile/${threads.userID}`} className="min-w-1/5">
-        <Image
-          src="/Cayde.jpg"
-          alt="User Profile Pic"
-          width={64}
-          height={64}
-          className="rounded-full"
-        />
-      </Link>
-      <Link href={`/threads/${threads.id}`} className="min-w-1/5">
-        <div className="w-64 xs:w-16 ">{threads?.title}</div>
-      </Link>
-      {/* {console.log(threads, "threads in cardrow")} */}
-      <Link href={`/user/profile/${threads.userID}`} className="min-w-1/5">
-        <div className="xs:w-25 w-24">{threads?.userID}</div>
-      </Link>
-      <div className="hidden min-w-1/5 md:block">{threads?.views}</div>
-      <div className="hidden min-w-1/5 md:block">{threads?.replies}</div>
+    <div className="my-2 flex items-center justify-around rounded-xl bg-white p-3 text-black">
+      <div className="flex items-center">
+        <div className="flex items-center">
+          {/* <UserIcon className="ml-3" height={24} width={24} /> */}
+          <Link
+            href={`/user/profile/${threads.userID}`}
+            className="min-w-1/5 mx-auto text-center"
+          >
+            <UserIcon className="mx-auto" height={24} width={24} />
+            <div className="xs:w-25 w-24">{threads?.userID}</div>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <Link href={`/user/profile/${threads.userID}`} className="min-w-1/5">
+            <Image
+              src="/Cayde.jpg"
+              alt="User Profile Pic"
+              width={96}
+              height={96}
+              className="rounded-md"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <ArrowRightOnRectangleIcon className="mx-2" height={16} width={16} />
+        <Link href={`/threads/${threads.id}`} className="min-w-1/5">
+          <div className="xs:w-16 w-64 ">{threads?.title}</div>
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <EyeIcon className="mx-2" height={16} width={16} />
+        <div className="min-w-1/5 hidden md:block">{threads?.views}</div>
+      </div>
+      <div className="flex items-center">
+        <ChatBubbleLeftIcon className="mx-2" height={16} width={16} />
+        <div className="min-w-1/5 hidden md:block">{threads?.replies}</div>
+      </div>
     </div>
   );
 };
